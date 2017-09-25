01a936fa381b81f7910d2664f4aa94639e893bb1
Colin Megill <colinmegill@gmail.com>
2017-09-25 11:43:19 -0700

    bar awaiting axes

--- src/components/lessons/01-bar.js
+++ src/components/lessons/01-bar.js
@@ -21,37 +21,42 @@ class Bar extends React.Component {
     return (
       <div>
         <Heading> Bar Chart </Heading>
-          {
-            mountains.map((mountain, i) => {
-              return (
-                <p>
-                  {`
-                  ${mountain.Mountain} is in the range
-                  ${mountain.Range} and it's
-                  ${mountain["Height (m)"]} high.
-                  `}
-                </p>
-              )
-            })
-          }
+          <svg
+            height={chartHeight}
+            width={chartWidth}
+            style={{border: "1px solid rgb(230,230,230)"}}>
+            <circle r={50}/>
+            <circle cx={200} cy={200} r={20}/>
+            {
+              mountains.map((mountain, i) => {
+                return (
+                  <rect
+                    fill={"rgba(0,0,0,1)"}
+                    x={i*6 + leftPadding}
+                    y={
+                      chartHeight -
+                      bottomPadding -
+                      y(mountain["Height (ft)"])
+                    }
+                    width={4}
+                    height={y(mountain["Height (ft)"])}/>
+                )
+              })
+            }
+          </svg>
       </div>
     );
   }
 }
 
 export default Bar;
+
 //
-// <svg
-//   height={chartHeight}
-//   width={chartWidth}
-//   style={{border: "1px solid rgb(230,230,230)"}}>
-// </svg>
+// <p>
+//   {`
+//   ${mountain.Mountain} is in the range
+//   ${mountain.Range} and it's
+//   ${mountain["Height (m)"]} high.
+//   `}
+// </p>
 //
-// return (
-//   <rect
-//     fill={"rgba(255,0,0,.2)"}
-//     x={i*6 + leftPadding}
-//     y={chartHeight - bottomPadding - y(mountain["Height (ft)"])}
-//     width={4}
-//     height={y(mountain["Height (ft)"])}/>
-// )
