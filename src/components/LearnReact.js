8015f329c36576cb24957cabb54bdab4fc36b516
Colin Megill <colinmegill@gmail.com>
2017-09-25 11:01:55 -0700

    two counters

--- src/components/LearnReact.js
+++ src/components/LearnReact.js
@@ -1,10 +1,16 @@
 import React from "react";
 
+const Counter2 = ({counter}) => {
+  return (
+    <div>As percent: {counter * 100}%</div>
+  );
+};
+
 class LearnReact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      counter: .51
     };
   }
 
@@ -12,6 +18,16 @@ class LearnReact extends React.Component {
     return (
       <div>
         Hello Learn React Component
+        <div>
+        As decimal: {this.state.counter}
+        </div>
+        <Counter2 counter={this.state.counter} />
+        <button
+          onClick={() => {
+            this.setState({counter: this.state.counter + .01})
+          }}>
+          Increment by .01
+        </button>
       </div>
     );
   }
