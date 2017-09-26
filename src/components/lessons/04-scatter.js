c84d48d3653afa4a762c02cb7e9c60277e6d07f6
Colin Megill <colinmegill@gmail.com>
2017-09-26 11:55:27 -0700

    moving state between two components

--- src/components/lessons/04-scatter.js
+++ src/components/lessons/04-scatter.js
@@ -14,89 +14,124 @@ import beetles from "../../data/beetles";
 import Heading from "../framework/heading";
 import {VictoryAxis, VictoryAnimation} from "victory";
 
-class Scatter extends React.Component {
+class CircleTwo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      x: Math.random() * 200,
-      y: Math.random() * 200,
-      r: Math.random() * 50,
+      r: 5
+    };
+  }
+  render() {
+    return (
+      <svg width={100} height={100}>
+        <circle
+          onClick={this.props.callbackFromParentForClick(this.state.r)}
+          fill={this.props.color}
+          cx={40}
+          cy={40}
+          r={this.state.r}/>
+      </svg>
+    )
+  }
+}
+
+class CircleOne extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      x: Math.random() * 50,
+      y: Math.random() * 50,
+      r: Math.random() * 20,
       color: '#'+(Math.random()*0xFFFFFF<<0).toString(16) // !
     };
   }
-
   render() {
     const leftPadding = 30;
     const bottomPadding = 30;
-    const chartHeight = 500;
-    const chartWidth = 1000;
-    const y = d3.scaleLinear().domain([0, 50000]).range([0, chartHeight])
+    const chartHeight = 300;
+    const chartWidth = 300;
+    return (
+      <div>
+      <button onClick={() => {
+          this.setState({
+            x: Math.random() * 200,
+            y: Math.random() * 200,
+            r: Math.random() * 50,
+            color: '#'+(Math.random()*0xFFFFFF<<0).toString(16) // !
+          })
+        }}>
+        Transition
+      </button>
+      <svg height={chartHeight} width={chartWidth} style={{border: "1px solid rgb(230,230,230)"}}>
+        <VictoryAnimation
+          data={{
+            x: this.state.x,
+            y: this.state.y,
+            r: this.state.r,
+            color: this.state.color
+          }}>
+          {(tween) => {
+            return (
+              <circle
+                onClick={this.props.callbackFromParentForClick(this.state.color)}
+                cx={tween.x}
+                cy={tween.y}
+                fill={tween.color}
+                r={ this.props.size ? this.props.size : tween.r}/>
+            )
+          }}
+        </VictoryAnimation>
+      </svg>
+      </div>
+    )
+  }
+}
+
+
+
+
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colorToTransfer: null,
+      sizeToTransfer: null
+    }
+  }
+
+  handleCircleOneClick(d) {
+    return () => {
+      console.log('circle 1 sees: ', d)
+      this.setState({colorToTransfer: d})
+    }
+  }
+
+  handleCircleTwoClick(r) {
+    return () => {
+      console.log('circle 2 sees: ', r)
+      this.setState({sizeToTransfer: r})
+    }
+  }
+
+  render() {
+    console.log('parent sees', this.state)
     return (
       <div>
         <Heading> Scatter </Heading>
-        <button onClick={() => {
-            this.setState({
-              x: Math.random() * 200,
-              y: Math.random() * 200,
-              r: Math.random() * 50,
-              color: '#'+(Math.random()*0xFFFFFF<<0).toString(16) // !
-            })
-          }}>
-          Transition
-        </button>
-        <svg height={chartHeight} width={chartWidth} style={{border: "1px solid rgb(230,230,230)"}}>
-          <VictoryAnimation
-            data={{
-              x: this.state.x,
-              y: this.state.y,
-              r: this.state.r,
-              color: this.state.color
-            }}>
-            {(tween) => {
-              return (
-                <circle
-                  cx={tween.x}
-                  cy={tween.y}
-                  fill={tween.color}
-                  r={tween.r}/>
-              )
-            }}
-          </VictoryAnimation>
-        </svg>
+        <CircleTwo
+          callbackFromParentForClick={this.handleCircleTwoClick.bind(this)}
+          color={
+            this.state.colorToTransfer ?
+            this.state.colorToTransfer : "rgb(250,0,0)"
+          }/>
+          <CircleOne
+            size={this.state.sizeToTransfer ? this.state.sizeToTransfer : null}
+            callbackFromParentForClick={this.handleCircleOneClick.bind(this)}/>
+
       </div>
     );
   }
 }
 
-export default Scatter;
-
-/*
-
-<VictoryAxis
-  padding={{
-    top: 0,
-    bottom: 0,
-    left: leftPadding, // cosmetic, 1px overhang, add +1 if persists
-    right: rightPadding // this is confusing, but ok
-  }}
-  domain={x.domain()}
-  offsetY={bottomPadding}
-  width={entropyChartWidth}
-  standalone={false}
-  label={"Position"}
-/>
-<VictoryAxis
-  dependentAxis
-  padding={{
-    top: 0,
-    bottom: bottomPadding,
-    left: leftPadding, // cosmetic, 1px overhang, add +1 if persists
-    right: rightPadding / 2 // bug? why is that / 2 necessary...
-  }}
-  domain={y.domain()}
-  offsetY={bottomPadding}
-  width={entropyChartWidth}
-  standalone={false}
-/>
-
-*/
+export default Parent;
