bd9f7431eea08879102c3a61d068699d1514c475
Colin Megill <colinmegill@gmail.com>
2017-09-27 11:31:59 -0700

    heatmap renders bw

--- src/components/lessons/06-hexbin.js
+++ src/components/lessons/06-hexbin.js
@@ -17,7 +17,8 @@ class Hexbin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      volcano: null
+      volcano: null,
+      canvasDrawn: false,
     };
   }
   componentWillMount () {
@@ -33,18 +34,41 @@ class Hexbin extends React.Component {
   componentDidUpdate() {
     let context;
     let image;
-    if (this.canvas) {
-      context = this.canvas.getContext("2d");
-      // image = context.createImageData(this.state.volcano.n, this.state.volcano.m);
-    }
+    if (this.canvas && !this.state.canvasDrawn) {
 
-    console.log("componentDidUpdate sees: ", this.canvas, context, image)
+      // const i0 =  d3.interpolateHsvLong(d3.hsv(120, 1, 0.65), d3.hsv(60, 1, 0.90));
+      // const i1 = d3.interpolateHsvLong(d3.hsv(60, 1, 0.90), d3.hsv(0, 0, 0.95));
+      // const interpolateTerrain = (t) => { return t < 0.5 ? i0(t * 2) : i1((t - 0.5) * 2); };
+      // const color = d3.scaleSequential(interpolateTerrain).domain([90, 190]);
+
+      const n = this.state.volcano.width;
+      const m = this.state.volcano.height;
+
+      context = this.canvas.getContext("2d");
+      image = context.createImageData(n, m);
+
+      for (var j = 0, k = 0, l = 0; j < m; ++j) {
+        for (var i = 0; i < n; ++i, ++k, l += 4) {
+          let v = this.state.volcano.values[k];
+                   image.data[l + 0] = v;
+                   image.data[l + 1] = v;
+                   image.data[l + 2] = v;
+                   image.data[l + 3] = 255;
+          // var c = d3.rgb(color(volcano.values[k]));
+          // image.data[l + 0] = c.r;
+          // image.data[l + 1] = c.g;
+          // image.data[l + 2] = c.b;
+          // image.data[l + 3] = 255;
+        }
+      }
+
+      context.putImageData(image, 0, 0);
+      this.setState({canvasDrawn: true})
+    }
   }
 
   render() {
     console.log('06-hexbin render sees', this.state, this.canvas)
-
-    // window.ourCanvas.
 
     return (
       <div>
@@ -57,7 +81,6 @@ class Hexbin extends React.Component {
             width={this.state.volcano.n}
             ref={(canvas) => {this.canvas = canvas;}}/>
         }
-
       </div>
     );
   }
