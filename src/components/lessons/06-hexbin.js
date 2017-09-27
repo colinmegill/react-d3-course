50786fa489ea069108a1ce1b06640c246dcf0eac
Colin Megill <colinmegill@gmail.com>
2017-09-27 10:57:34 -0700

    canvas / refs

--- src/components/lessons/06-hexbin.js
+++ src/components/lessons/06-hexbin.js
@@ -14,22 +14,61 @@ import mountains from "../../data/mountains";
 import Heading from "../framework/heading";
 
 class Hexbin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      volcano: null
+    };
+  }
+  componentWillMount () {
+    const url = "https://gist.githubusercontent.com/mbostock/3074470/raw/472f0f412886340711f6b06cc2eebd5665538a95/volcano.json";
+    d3.json(url, (error, volcano) => {
+      if (!error) {
+        this.setState({volcano})
+      } else {
+        console.log(error);
+      }
+    })
+  }
+  componentDidUpdate() {
+    let context;
+    let image;
+    if (this.canvas) {
+      context = this.canvas.getContext("2d");
+      // image = context.createImageData(this.state.volcano.n, this.state.volcano.m);
+    }
+
+    console.log("componentDidUpdate sees: ", this.canvas, context, image)
+  }
+
   render() {
-    console.log(mountains)
-    const leftPadding = 30;
-    const bottomPadding = 30;
-    const chartHeight = 800;
-    const chartWidth = 1000;
-    const y = d3.scaleLinear().domain([0, 50000]).range([0, chartHeight])
+    console.log('06-hexbin render sees', this.state, this.canvas)
+
+    // window.ourCanvas.
+
     return (
       <div>
         <Heading> Hex bins </Heading>
-        <svg height={chartHeight} width={chartWidth} style={{border: "1px solid rgb(230,230,230)"}}>
+        {
+          !this.state.volcano ? null :
+          <canvas
+            style={{border: "1px solid green"}}
+            height={this.state.volcano.m}
+            width={this.state.volcano.n}
+            ref={(canvas) => {this.canvas = canvas;}}/>
+        }
 
-        </svg>
       </div>
     );
   }
 }
 
 export default Hexbin;
+
+// const leftPadding = 30;
+// const bottomPadding = 30;
+// const chartHeight = 800;
+// const chartWidth = 1000;
+// <svg height={chartHeight} width={chartWidth} style={{border: "1px solid rgb(230,230,230)"}}>
+//
+// </svg>
