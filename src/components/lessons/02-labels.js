7337d14043c96b771e9660c9e82f993e026dbe8e
Colin Megill <colinmegill@gmail.com>
2017-09-28 11:46:47 -0700

    reducer and map

--- src/components/lessons/02-labels.js
+++ src/components/lessons/02-labels.js
@@ -2,10 +2,11 @@ import React from "react";
 import {connect} from "react-redux";
 
 @connect((state) => {
-  console.log(state)
+  console.log("connect in labels sees: ", state)
   return {
     radius: state.kittenReducer.radius,
-    color: state.kittenReducer.color
+    color: state.kittenReducer.color,
+    x: state.kittenReducer.x
   };
 })
 class Labels extends React.Component {
@@ -14,18 +15,42 @@ class Labels extends React.Component {
       type: "KITTEN"
     });
   }
+  handleMoveRight() {
+    this.props.dispatch({
+      type: "MOVE_RIGHT"
+    })
+  }
+
+  componentDidMount() {
+    console.log('map object', L)
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+
+    L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1IjoiY29saW5tZWdpbGwiLCJhIjoiY2o4NHNyMjhqMGRkbDJxb2FvdnRxemN4aSJ9.u3xAok69HzQ7loKdk83NaA',
+    }).addTo(mymap);
+
+
+
+  }
+
   render() {
 
     return (
       <div>
+        <div style={{height: 500, width: 500, border: "1px solid green"}} id="mapid"></div>
         <button onClick={this.handleClick.bind(this)}> Radius ++ </button>
-      <svg height={500} width={800}>
-        <circle
-          fill={this.props.radius < 40 ? "red" : this.props.color }
-          cx={200}
-          cy={200}
-          r={this.props.radius}/>
-      </svg>
+        <button onClick={this.handleMoveRight.bind(this)}> ➵ </button>
+        <svg height={500} width={500}>
+          <circle
+            fill={this.props.radius < 40 ? "red" : this.props.color }
+            cx={this.props.x}
+            cy={200}
+            r={this.props.radius}/>
+        </svg>
       </div>
     );
   }
--- src/reducers/kitten.js
+++ src/reducers/kitten.js
@@ -1,14 +1,22 @@
 import * as types from "../actions";
 
-const kittenReducer = (state = {
+const initialState = {
   radius: 30,
-  color: "blue"
-}, action) => {
+  color: "blue",
+  x: 200,
+  y: 200,
+}
+
+const kittenReducer = (state = initialState, action) => {
   switch (action.type) {
   case "KITTEN":
     return Object.assign({}, state, {
       radius: state.radius + 2
     });
+  case "MOVE_RIGHT":
+    return Object.assign({}, state, {
+      x: state.x + 20
+    })
   default:
     return state;
   }
